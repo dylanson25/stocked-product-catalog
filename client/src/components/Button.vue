f899a7b4e221565f6494739e243f2d7f4884a077
Dylanson25 <dvillarreal0@ucol.mx>
2022-10-28 22:40:10 -0500

    add/Button component

--- client/src/components/Button.vue
+++ client/src/components/Button.vue
@@ -1,5 +1,30 @@
 <template>
-  <h1>boton</h1>
+  <b-skeleton v-if="skeletonLoading" class="button" />
+  <b-button v-else v-bind="{ ...$attrs, ...$props }" @click.stop="onClick">
+    <slot />
+  </b-button>
 </template>
-<script></script>
-<style lang="sass" scoped></style>
+<script>
+export default {
+  methods: {
+    onClick(e) {
+      this.$emit("click", e);
+    },
+  },
+  props: {
+    disabled: { type: Boolean, default: false },
+    skeletonLoading: { type: Boolean, default: false },
+  },
+};
+</script>
+<style lang="sass" scoped>
+.b-skeleton
+  border: none
+  margin-top: 0px
+  padding: 0
+  width: auto
+  ::v-deep
+    .b-skeleton-item
+      min-height: 40px
+      min-width: 70px
+</style>
