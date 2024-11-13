22078b7031e168bb77c6f82a75d836f0dcdf810d
Dylanson25 <dylanson25@users.noreply.github.com>
2024-11-13 14:42:24 -0800

    [#24] -  button base styles

--- client/tailwind-plugins/components/tailwindButton.js
+++ client/tailwind-plugins/components/tailwindButton.js
@@ -1,14 +1,44 @@
-const buttonComponent =   function({  addUtilities, theme, e}) {
-      const sizes = theme('sizes');
+const buttonComponent =   function({ addComponents, addUtilities, theme, e}) {
+  const sizes = theme('sizes');
 
-      const btnSizeVariants = Object.keys(sizes).reduce((acc, key) => {
-        acc[`.btn--${e(key)}`] = {
-            fontSize: sizes[key],
-          };
-          return acc;
-      }, {})
+  const btnSizeVariants = Object.keys(sizes).reduce((acc, key) => {
+    acc[`.btn--${e(key)}`] = {
+        fontSize: sizes[key],
+      };
+      return acc;
+  }, {})
+  addUtilities(btnSizeVariants, ['responsive'])
 
-      addUtilities(btnSizeVariants, ['responsive'])
+  addComponents({
+    '.btn': {
+        borderColor: '#D1D5DB',
+        color: '#1F2937',
+        position: 'relative',
+        margin: '0',
+        display: 'inline-flex',
+        cursor: 'pointer',
+        alignItems: 'center',
+        justifyContent: 'center',
+        whiteSpace: 'nowrap',
+        borderRadius: '0.375rem',
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        padding: '0.75rem',
+        textAlign: 'center',
+        verticalAlign: 'top',
+        fontSize: '1rem',
+        fontWeight: '400',
+        boxShadow: 'none',
+        textDecoration: 'none',
+	     background: 'transparent',
+	     height: '2.5em',
+        '&.btn--disabled': {
+          pointerEvents: 'none',
+          cursor: 'not-allowed',
+          opacity: '0.5',
+        }
+      },
+  })
 }
 
 module.exports = buttonComponent;
