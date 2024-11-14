c2ac4dfdd30b667d309223086f04af4158751ce5
Dylanson25 <dylanson25@users.noreply.github.com>
2024-11-14 12:48:40 -0800

    [#24] - button inverted variant

--- client/tailwind-plugins/components/tailwindButton.js
+++ client/tailwind-plugins/components/tailwindButton.js
@@ -1,71 +1,80 @@
-const { white } = require("tailwindcss/colors");
+const { white } = require('tailwindcss/colors');
 
-const buttonComponent =   function({ addComponents, addUtilities, theme, e}) {
-  const sizes = theme('sizes');
-  const colors = theme('colors');
+const buttonComponent = function ({ addComponents, addUtilities, theme, e }) {
+	const sizes = theme('sizes');
+	const colors = theme('colors');
 
-  const btnSizeVariants = Object.keys(sizes).reduce((acc, key) => {
-    acc[`.btn--${e(key)}`] = {
-        fontSize: sizes[key],
-    };
+	const btnSizeVariants = Object.keys(sizes).reduce((acc, key) => {
+		acc[`.btn--${e(key)}`] = {
+			fontSize: sizes[key],
+		};
 
-    return acc;
-  }, {})
+		return acc;
+	}, {});
 
-  const btnColorsVariants = Object.keys(colors).reduce((acc, key) => {
-    acc[`&.btn--${e(key)}`] = {
-      backgroundColor: colors[key]['400'],
-      color: white,
-      borderColor: 'transparent',
-      '&:hover': {
-        filter: 'brightness(95.5%)',
-      },
-      '&:focus': {
-        boxShadow: `0 0 0 .25rem ${colors[key]['50']}`,
-      },
-      '&.btn--outlined':{
-        borderColor: colors[key]['500'],
-        backgroundColor: 'transparent',
-        color: colors[key]['500']
-      }
-    };
+	const btnColorsVariants = Object.keys(colors).reduce((acc, key) => {
+		acc[`&.btn--${e(key)}`] = {
+			backgroundColor: colors[key]['400'],
+			color: white,
+			borderColor: 'transparent',
+			'&:hover': {
+				filter: 'brightness(95.5%)',
+			},
+			'&:focus': {
+				boxShadow: `0 0 0 .25rem ${colors[key]['50']}`,
+			},
+			'&.btn--outlined': {
+				borderColor: colors[key]['500'],
+				backgroundColor: 'transparent',
+				color: colors[key]['500'],
+				'&:hover': {
+					backgroundColor: colors[key]['500'],
+					color: white,
+				},
+			},
+			'&.btn--inverted': {
+				borderColor: 'transparent',
+				backgroundColor: white,
+				color: colors[key]['500'],
+			},
+		};
 
-    return acc;
-  }, {})
+		return acc;
+	}, {});
 
-  addUtilities(btnSizeVariants, ['responsive'])
+	addUtilities(btnSizeVariants, ['responsive']);
 
-  addComponents({
-    '.btn': {
-	    background: 'transparent',
-	    height: '2.5em',
-      alignItems: 'center',
-      borderColor: '#D1D5DB',
-      borderRadius: '0.375rem',
-      borderStyle: 'solid',
-      borderWidth: '1px',
-      boxShadow: 'none',
-      color: '#1F2937',
-      cursor: 'pointer',
-      display: 'inline-flex',
-      fontSize: '1rem',
-      fontWeight: '400',
-      justifyContent: 'center',
-      margin: '0',
-      padding: '0.75rem',
-      position: 'relative',
-      textAlign: 'center',
-      textDecoration: 'none',
-      verticalAlign: 'top',
-      whiteSpace: 'nowrap',
-       '&.btn--disabled': {
-          pointerEvents: 'none',
-          cursor: 'not-allowed',
-          opacity: '0.5',
-      },
-      ...btnColorsVariants
-    },
-  })
-}
+	addComponents({
+		'.btn': {
+			background: 'transparent',
+			height: '2.5em',
+			alignItems: 'center',
+			borderColor: '#D1D5DB',
+			borderRadius: '0.375rem',
+			borderStyle: 'solid',
+			borderWidth: '1px',
+			boxShadow: 'none',
+			color: '#1F2937',
+			cursor: 'pointer',
+			display: 'inline-flex',
+			fontSize: '1rem',
+			fontWeight: '400',
+			justifyContent: 'center',
+			margin: '0',
+			padding: '0.75rem',
+			position: 'relative',
+			textAlign: 'center',
+			textDecoration: 'none',
+			verticalAlign: 'top',
+			whiteSpace: 'nowrap',
+			'&.btn--disabled': {
+				pointerEvents: 'none',
+				cursor: 'not-allowed',
+				opacity: '0.5',
+			},
+			...btnColorsVariants,
+		},
+	});
+};
 
 module.exports = buttonComponent;
