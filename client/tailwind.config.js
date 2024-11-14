83a9cc5246d6379760a77d19ba88b17cdb26fe66
Dylanson25 <dylanson25@users.noreply.github.com>
2024-11-14 13:16:08 -0800

    [#24] - Update  tailwind config to add button

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const colors = require('tailwindcss/colors');
+const buttonComponent = require('./tailwind-plugins/components/tailwindButton');
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -12,8 +14,15 @@ export default {
 			info: colors.cyan,
 			warning: colors.yellow,
 			danger: colors.rose,
+			gray: colors.gray,
 		},
-		extend: {},
+		extend: {
+			sizes: {
+				small: '0.75rem',
+				medium: '1.20rem',
+				large: '1.40rem',
+			},
+		},
 	},
-	plugins: [],
+	plugins: [buttonComponent],
 };
