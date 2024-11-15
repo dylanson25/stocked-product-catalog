7b1a44a6ddafb31d1a90c0921c39d015d8096194
Dylanson25 <dylanson25@users.noreply.github.com>
2024-11-15 12:37:32 -0800

    [#24] - Class types nedded

--- client/src/types/config.ts
+++ client/src/types/config.ts
@@ -1,6 +1,12 @@
 export type ComponentClass = string | Array<string> | ClassFunction;
 
+export type ClassObject = {
+	class: ComponentClass;
+};
+
 export type ComponentProps = Record<string, unknown>;
+
+export type ClassDefinition = ComponentClass | ClassObject;
 
 export type ClassFunction = (
 	suffix: string,
